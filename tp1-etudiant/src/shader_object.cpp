24bf74d134d9e8888f0d569f9b13e77959021d81
Erwann <robarderwann@gmail.com>
2025-01-21 13:13:01 -0500

    initial project push

---
+++ tp1-etudiant/src/shader_object.cpp
@@ -0,0 +1,33 @@
+#include "shader_object.h"
+
+#include <iostream>
+
+ShaderObject::ShaderObject(GLenum type, const char* code)
+{
+    // TODO
+}
+    
+ShaderObject::~ShaderObject()
+{
+    // TODO
+}
+
+GLuint ShaderObject::id()
+{
+    // TODO
+}
+
+void ShaderObject::checkCompilingError()
+{
+    GLint success;
+    GLchar infoLog[1024];
+
+    glGetShaderiv(m_id, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(m_id, 1024, NULL, infoLog);
+        glDeleteShader(m_id);
+        std::cout << "Shader compile error: " << infoLog << std::endl;
+    }
+}
+
